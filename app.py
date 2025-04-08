f952624db063e411d7f4be840740ec46a09150cf
SHELBY <75433486+whoshelby@users.noreply.github.com>
2025-04-08 20:50:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ HTML_TEMP = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>ReadmeRiser</title>
+    <title>README Generator</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body { background-color: #f8f9fa; padding: 40px; }
